5cec27ffe30a8b803608f4366adf807d08e21921
JjayFabor <faborjaylordvhan@gmail.com>
2024-12-28 22:02:48 +0800

    Update migration

--- api/user/models.py
+++ api/user/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import models as auth_models
-from phonenumber_field.modelfields import PhoneNumberField
 
 
 class UserManager(auth_models.BaseUserManager):
